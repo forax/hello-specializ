e48577ff029dc294b027cfa1306ef935503dfd21
Remi Forax <forax@univ-mlv.fr>
2015-08-23 16:03:21 +0200

    initial commit

---
+++ src/specializ/Dumper.java
@@ -0,0 +1,362 @@
+package specializ;
+
+import static org.objectweb.asm.Opcodes.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Handle;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+
+public class Dumper {
+  public static byte[] dumpArrayList() {
+    ClassWriter cw = new ClassWriter(0);
+    FieldVisitor fv;
+    MethodVisitor mv;
+
+    cw.newUTF8("$[E");       // 1: L[java/lang/Object;
+    cw.newUTF8("$(I)[E");    // 2: (I)L[java/lang/Object;
+    cw.newUTF8("$E");        // 3: Ljava.lang.Object;
+    cw.newUTF8("$(E)V");     // 4: (Ljava/lang/Object;)V
+    cw.newUTF8("$([E)I");    // 5: ([Ljava/lang/Object;)I
+    cw.newUTF8("$([EI)[E");  // 6: (L[java/lang/Object;I)[Ljava/lang/Object;
+    cw.newUTF8("$([EIE)V");  // 7: (L[java/lang/Object;ILjava/lang/Object;)V
+    cw.newUTF8("$(I)E");     // 8: (I)Ljava/lang/Object;
+    cw.newUTF8("$([EI)E");   // 9: (L[java/lang/Object;I)Ljava/lang/Object;
+    cw.newUTF8("$(IE)V");    //10: (ILjava/lang/Object;)V
+    cw.newUTF8("$(E)Z");     //11: (Ljava/lang/Object;)Z
+    cw.newUTF8("$(EE)Z");    //12: (Ljava/lang/Object;Ljava/lang/Object;)Z
+    
+    Handle BSM = new Handle(H_INVOKESTATIC, "specializ/java/util/SpecializMetaFactory", "bsm",
+        "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;)Ljava/lang/invoke/CallSite;");
+    
+    cw.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "specializ/java/util/ArrayList", "<E:Ljava/lang/Object;>Ljava/lang/Object;", "java/lang/Object", null);
+
+    {
+      fv = cw.visitField(ACC_PRIVATE, "data", "$[E", null, null);
+      fv.visitEnd();
+    }
+    {
+      fv = cw.visitField(ACC_PRIVATE, "size", "I", null, null);
+      fv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitIntInsn(BIPUSH, 16);
+      //mv.visitIntInsn(BIPUSH, 1);  // test resize()
+      mv.visitInvokeDynamicInsn("newArrayInstance", "$(I)[E", BSM, "$E");
+      mv.visitFieldInsn(PUTFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(3, 1);
+      mv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "add", "$(E)V", "(TE;)V", null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitInvokeDynamicInsn("arrayLength", "$([E)I", BSM, "$E");
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "size", "I");
+      Label l0 = new Label();
+      mv.visitJumpInsn(IF_ICMPNE, l0);
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitMethodInsn(INVOKESPECIAL, "specializ/java/util/ArrayList", "resize", "()V", false);
+      mv.visitLabel(l0);
+      mv.visitFrame(F_SAME, 0, null, 0, null);
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitInsn(DUP);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "size", "I");
+      mv.visitInsn(DUP_X1);
+      mv.visitInsn(ICONST_1);
+      mv.visitInsn(IADD);
+      mv.visitFieldInsn(PUTFIELD, "specializ/java/util/ArrayList", "size", "I");
+      uload(mv, 1, "$E");
+      mv.visitInvokeDynamicInsn("arraySet", "$([EIE)V", BSM, "$E");
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(5, 3);   // locals: 1 + 2 (maybe a long)
+      mv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PRIVATE, "resize", "()V", null, null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "size", "I");
+      mv.visitInsn(ICONST_1);
+      mv.visitInsn(ISHL);
+      mv.visitInvokeDynamicInsn("arrayCopyOf", "$([EI)[E", BSM, "$E");
+      mv.visitFieldInsn(PUTFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(4, 1);
+      mv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "get", "$(I)E", "(I)TE;", null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitVarInsn(ILOAD, 1);
+      mv.visitInvokeDynamicInsn("arrayGet", "$([EI)E", BSM, "$E");
+      ureturn(mv, "$E");
+      mv.visitMaxs(2, 2);
+      mv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "set", "$(IE)V", "(ITE;)V", null);
+      mv.visitCode();
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitVarInsn(ILOAD, 1);
+      uload(mv, 2, "$E");
+      mv.visitInvokeDynamicInsn("arraySet", "$([EIE)V", BSM, "$E");
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(4, 4);   // stacks: 1 + 1 + 2 locals: 1 + 1 + 2 
+      mv.visitEnd();
+    }
+    {
+      mv = cw.visitMethod(ACC_PUBLIC, "contains", "$(E)Z", "(TE;)Z", null);
+      mv.visitCode();
+      mv.visitInsn(ICONST_0);
+      mv.visitVarInsn(ISTORE, 3);
+      Label l0 = new Label();
+      mv.visitJumpInsn(GOTO, l0);
+      Label l1 = new Label();
+      mv.visitLabel(l1);
+      mv.visitFrame(F_APPEND,1, new Object[] { INTEGER }, 0, null);
+      uload(mv, 1, "$E");
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "data", "$[E");
+      mv.visitVarInsn(ILOAD, 3);
+      mv.visitInvokeDynamicInsn("arrayGet", "$([EI)E", BSM, "$E");
+      mv.visitInvokeDynamicInsn("equals", "$(EE)Z", BSM, "$E");
+      Label l2 = new Label();
+      mv.visitJumpInsn(IFEQ, l2);
+      mv.visitInsn(ICONST_1);
+      mv.visitInsn(IRETURN);
+      mv.visitLabel(l2);
+      mv.visitFrame(F_SAME, 0, null, 0, null);
+      mv.visitIincInsn(3, 1);
+      mv.visitLabel(l0);
+      mv.visitFrame(F_SAME, 0, null, 0, null);
+      mv.visitVarInsn(ILOAD, 3);
+      mv.visitVarInsn(ALOAD, 0);
+      mv.visitFieldInsn(GETFIELD, "specializ/java/util/ArrayList", "size", "I");
+      mv.visitJumpInsn(IF_ICMPLT, l1);
+      mv.visitInsn(ICONST_0);
+      mv.visitInsn(IRETURN);
+      mv.visitMaxs(4, 4);
+      mv.visitEnd();
+    }
+    cw.visitEnd();
+
+    return cw.toByteArray();
+  }
+  
+  private static void uload(MethodVisitor mv, int slot, String desc) {
+    Label jumpI = new Label();
+    Label jumpL = new Label();
+    Label jumpF = new Label();
+    Label jumpD = new Label();
+    Label end = new Label();
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("I");
+    mv.visitJumpInsn(IF_ACMPNE, jumpI);
+    mv.visitVarInsn(ILOAD, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpI);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("J");
+    mv.visitJumpInsn(IF_ACMPNE, jumpL);
+    mv.visitVarInsn(LLOAD, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpL);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("F");
+    mv.visitJumpInsn(IF_ACMPNE, jumpF);
+    mv.visitVarInsn(FLOAD, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpF);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("D");
+    mv.visitJumpInsn(IF_ACMPNE, jumpD);
+    mv.visitVarInsn(DLOAD, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpD);
+    
+    mv.visitVarInsn(ALOAD, slot);
+    mv.visitLabel(end);
+  }
+  
+  private static void ustore(MethodVisitor mv, int slot, String desc) {
+    Label jumpI = new Label();
+    Label jumpL = new Label();
+    Label jumpF = new Label();
+    Label jumpD = new Label();
+    Label end = new Label();
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("I");
+    mv.visitJumpInsn(IF_ACMPNE, jumpI);
+    mv.visitVarInsn(ISTORE, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpI);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("J");
+    mv.visitJumpInsn(IF_ACMPNE, jumpL);
+    mv.visitVarInsn(LSTORE, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpL);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("F");
+    mv.visitJumpInsn(IF_ACMPNE, jumpF);
+    mv.visitVarInsn(FSTORE, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpF);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("D");
+    mv.visitJumpInsn(IF_ACMPNE, jumpD);
+    mv.visitVarInsn(DSTORE, slot);
+    mv.visitJumpInsn(GOTO, end);
+    mv.visitLabel(jumpD);
+    
+    mv.visitVarInsn(ALOAD, slot);
+    mv.visitLabel(end);
+  }
+  
+  private static void ureturn(MethodVisitor mv, String desc) {
+    Label jumpI = new Label();
+    Label jumpL = new Label();
+    Label jumpF = new Label();
+    Label jumpD = new Label();
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("I");
+    mv.visitJumpInsn(IF_ACMPNE, jumpI);
+    mv.visitInsn(IRETURN);
+    mv.visitLabel(jumpI);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("J");
+    mv.visitJumpInsn(IF_ACMPNE, jumpL);
+    mv.visitInsn(LRETURN);
+    mv.visitLabel(jumpL);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("F");
+    mv.visitJumpInsn(IF_ACMPNE, jumpF);
+    mv.visitInsn(FRETURN);
+    mv.visitLabel(jumpF);
+    
+    mv.visitLdcInsn(desc);
+    mv.visitLdcInsn("D");
+    mv.visitJumpInsn(IF_ACMPNE, jumpD);
+    mv.visitInsn(DRETURN);
+    mv.visitLabel(jumpD);
+    
+    mv.visitInsn(ARETURN);
+  }
+  
+  public static byte[] dumpTest() {
+    ClassWriter cw = new ClassWriter(0);
+    MethodVisitor mv;
+
+    Handle INDY = new Handle(H_INVOKESTATIC, "specializ/java/util/SpecializMetaFactory", "indy",
+        "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;Ljava/lang/String;)Ljava/lang/invoke/CallSite;");
+    
+    cw.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "specializ/Test", null, "java/lang/Object", null);
+
+    {
+      mv = cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+      mv.visitCode();
+      mv.visitInvokeDynamicInsn("new", "()Ljava/lang/Object;", INDY, "specializ/java/util/ArrayList", "L");
+      mv.visitVarInsn(ASTORE, 1);
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn("foo");
+      mv.visitInvokeDynamicInsn("add", "(Ljava/lang/Object;Ljava/lang/Object;)V", INDY, "specializ/java/util/ArrayList", "L");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn("bar");
+      mv.visitInvokeDynamicInsn("add", "(Ljava/lang/Object;Ljava/lang/Object;)V", INDY, "specializ/java/util/ArrayList", "L");
+      mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitInsn(ICONST_0);
+      mv.visitInvokeDynamicInsn("get", "(Ljava/lang/Object;I)Ljava/lang/Object;", INDY, "specializ/java/util/ArrayList", "L");
+      mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false);
+      mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn("baz");
+      mv.visitInvokeDynamicInsn("contains", "(Ljava/lang/Object;Ljava/lang/Object;)Z", INDY, "specializ/java/util/ArrayList", "L");
+      mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Z)V", false);
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(3, 2);
+      mv.visitEnd();
+    }
+    cw.visitEnd();
+
+    return cw.toByteArray();
+  }
+  
+  public static byte[] dumpTest2() {
+    ClassWriter cw = new ClassWriter(0);
+    MethodVisitor mv;
+
+    Handle INDY = new Handle(H_INVOKESTATIC, "specializ/java/util/SpecializMetaFactory", "indy",
+        "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;Ljava/lang/String;)Ljava/lang/invoke/CallSite;");
+    
+    cw.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "specializ/Test2", null, "java/lang/Object", null);
+
+    {
+      mv = cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+      mv.visitCode();
+      mv.visitInvokeDynamicInsn("new", "()Ljava/lang/Object;", INDY, "specializ/java/util/ArrayList", "J");
+      mv.visitVarInsn(ASTORE, 1);
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn(1L);
+      mv.visitInvokeDynamicInsn("add", "(Ljava/lang/Object;J)V", INDY, "specializ/java/util/ArrayList", "J");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn(2L);
+      mv.visitInvokeDynamicInsn("add", "(Ljava/lang/Object;J)V", INDY, "specializ/java/util/ArrayList", "J");
+      mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitInsn(ICONST_0);
+      mv.visitInvokeDynamicInsn("get", "(Ljava/lang/Object;I)J", INDY, "specializ/java/util/ArrayList", "J");
+      mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(J)V", false);
+      mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+      mv.visitVarInsn(ALOAD, 1);
+      mv.visitLdcInsn(3L);
+      mv.visitInvokeDynamicInsn("contains", "(Ljava/lang/Object;J)Z", INDY, "specializ/java/util/ArrayList", "J");
+      mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Z)V", false);
+      mv.visitInsn(RETURN);
+      mv.visitMaxs(4, 2);
+      mv.visitEnd();
+    }
+    cw.visitEnd();
+
+    return cw.toByteArray();
+  }
+  
+  public static void main(String[] args) throws IOException {
+    Files.write(Paths.get("classes/specializ/java/util/ArrayList.class"), dumpArrayList());
+    Files.write(Paths.get("classes/specializ/Test.class"), dumpTest());
+    Files.write(Paths.get("classes/specializ/Test2.class"), dumpTest2());
+  }
+}
---
+++ src/specializ/java/util/SpecializMetaFactory.java
@@ -0,0 +1,215 @@
+package specializ.java.util;
+
+import static java.lang.invoke.MethodHandles.publicLookup;
+import static java.lang.invoke.MethodType.methodType;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.IntStream.range;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.CallSite;
+import java.lang.invoke.ConstantCallSite;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Objects;
+
+import sun.misc.Unsafe;
+
+public class SpecializMetaFactory {
+  private static final Unsafe UNSAFE;
+  static {
+     try {
+      Field field = Unsafe.class.getDeclaredField("theUnsafe");
+      field.setAccessible(true);
+      UNSAFE = (Unsafe)field.get(null);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+  
+  private static final MethodHandle newArrayInstance, arrayLength,
+                                    arrayGets[], arraySets[], arrayCopyOfs[], equals[];
+  static {
+    newArrayInstance = findStatic(Array.class, "newInstance", Object.class, Class.class, int.class);
+    arrayLength = findStatic(Array.class, "getLength", int.class, Object.class);
+
+    VariantKind[] kinds = VariantKind.values();
+    String[] arrayGetName = { "get", "getInt", "getLong", "getFloat", "getDouble" };
+    arrayGets = range(0, kinds.length).mapToObj(i -> 
+      findStatic(Array.class, arrayGetName[i], kinds[i].type, Object.class, int.class)).toArray(MethodHandle[]::new);
+    String[] arraySetName = { "set", "setInt", "setLong", "setFloat", "setDouble" };
+    arraySets = range(0, kinds.length).mapToObj(i -> 
+      findStatic(Array.class, arraySetName[i], void.class, Object.class, int.class, kinds[i].type)).toArray(MethodHandle[]::new);
+    arrayCopyOfs = range(0, kinds.length).mapToObj(i -> 
+      findStatic(Arrays.class, "copyOf", kinds[i].arrayType, kinds[i].arrayType, int.class)).toArray(MethodHandle[]::new);
+    equals = range(0, kinds.length).mapToObj(i -> 
+      findStatic(SpecializMetaFactory.class, "equals", boolean.class, kinds[i].type, kinds[i].type)).toArray(MethodHandle[]::new);
+  }
+  
+  private static MethodHandle findStatic(Class<?> type, String name, Class<?> returnType, Class<?>... parameterTypes) {
+    try {
+      return publicLookup().findStatic(type, name, methodType(returnType, parameterTypes));
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+  
+  public static boolean equals(Object o1, Object o2) {
+    return Objects.equals(o1, o2);
+  }
+  public static boolean equals(int i1, int i2) {
+    return i1 == i2;
+  }
+  public static boolean equals(long l1, long l2) {
+    return l1 == l2;
+  }
+  public static boolean equals(float f1, float f2) {
+    return f1 == f2;
+  }
+  public static boolean equals(double d1, double d2) {
+    return d1 == d2;
+  }
+  
+  public static CallSite bsm(Lookup lookup, String name, MethodType type, String variant) {
+    VariantKind kind = VariantKind.valueOf(variant.substring(0, 1));  // FIXME if several arguments !
+    MethodHandle mh;
+    switch(name) {
+    case "newArrayInstance":
+      mh = MethodHandles.insertArguments(newArrayInstance, 0, kind.type);
+      break;
+    case "arrayLength":
+      mh = arrayLength;
+      break;
+    case "arrayGet":
+      mh = arrayGets[kind.ordinal()];
+      break;
+    case "arraySet":
+      mh = arraySets[kind.ordinal()];
+      break;
+    case "arrayCopyOf":
+      mh = arrayCopyOfs[kind.ordinal()];
+      break;
+    case "equals":
+      mh = equals[kind.ordinal()];
+      break;
+    default:
+      throw new LinkageError("bsm from " + lookup.lookupClass().getName() + " " + name + type + " variant:" + variant);
+    }
+    
+    return new ConstantCallSite(mh.asType(type));
+  }
+  
+  enum VariantKind {
+    L(Object.class, Object[].class),
+    I(int.class, int[].class),
+    J(long.class, long[].class),
+    F(float.class, float[].class),
+    D(double.class, double[].class);
+    
+    final Class<?> type;
+    final Class<?> arrayType;
+    
+    private VariantKind(Class<?> type, Class<?> arrayType) {
+      this.type = type;
+      this.arrayType = arrayType;
+    }
+    
+    String getReplacementDesc() {
+      if (this == L) {
+        return "Ljava/lang/Object;";
+      }
+      return name();
+    }
+  }
+  
+  static class GenericClass {
+    private final byte[] code;
+    private final EnumMap<VariantKind, Class<?>> variantMap = new EnumMap<>(VariantKind.class);
+    
+    GenericClass(byte[] code) {
+      this.code = requireNonNull(code);
+    }
+    
+    public Class<?> getVariant(VariantKind kind) {
+      return variantMap.computeIfAbsent(kind, this::specialize);
+    }
+    
+    private static final byte UTF8_CONSTANT_POOL_TAG = 1;
+    
+    private Class<?> specialize(VariantKind kind) {
+      // do the translation for the first constant pool entries !
+      byte[] code = this.code;
+      
+      int entryCount = ((code[8] & 0xFF) << 8) | (code[9] & 0xFF);
+      Object[] constants = new Object[entryCount];
+      
+      int index = 10;
+      for(int i = 1; i < entryCount; i++) {
+        if (code[index] == UTF8_CONSTANT_POOL_TAG) {
+          int length = ((code[index + 1] & 0xFF) << 8) | (code[index + 2] & 0xFF); 
+          
+          //FIXME write a UTF8 decoding method !
+          String key;
+          try {
+            key = new DataInputStream(new ByteArrayInputStream(code, index + 1, code.length - index - 1)).readUTF();
+          } catch (IOException e) {
+            throw new AssertionError(e);
+          }
+          
+          if (key.charAt(0) == '$') {
+            constants[i] = key.substring(1).replace("E", kind.getReplacementDesc());
+            index += 1 + 2 + length;
+            
+            //System.out.println("key " + key + " is substituted by " + constants[i]);
+            continue;
+          }
+        }
+        break;
+      }
+      
+      //System.out.println(Arrays.toString(constants));
+      
+      return UNSAFE.defineAnonymousClass(SpecializMetaFactory.class /* TODO revisit*/, code, constants);
+    }
+    
+    static GenericClass create(Class<?> clazz, String name) {
+      byte[] code = new byte[8192];
+      int offset = 0;
+      try(InputStream input = clazz.getClassLoader().getResourceAsStream(name + ".class")) {
+        int read;
+        while((read = input.read(code, offset, code.length - offset)) != -1) {
+          offset += read;
+          if (offset > code.length >>> 1) {
+            code = Arrays.copyOf(code, code.length << 1);
+          }
+        }
+      } catch (IOException e) {
+        throw (LinkageError)new LinkageError().initCause(e);
+      }
+      return new GenericClass(Arrays.copyOf(code, offset)); // shrink to the right size
+    }
+  }
+  
+  private static final HashMap<String, GenericClass> GENERIC_MAP = new HashMap<>();
+  
+  public static CallSite indy(Lookup lookup, String name, MethodType type, String generics, String arguments) throws NoSuchMethodException, IllegalAccessException {
+    VariantKind kind = VariantKind.valueOf(arguments); // FIXME if several arguments !
+    Class<?> variant = GENERIC_MAP.computeIfAbsent(generics, className -> GenericClass.create(lookup.lookupClass(), className)).getVariant(kind);
+    MethodHandle mh;
+    if ("new".equals(name)) {
+      mh = lookup.findConstructor(variant, type.changeReturnType(void.class)); 
+    } else {
+      mh = lookup.findVirtual(variant, name, type.dropParameterTypes(0, 1));
+    }
+    return new ConstantCallSite(mh.asType(type));
+  }
+}
